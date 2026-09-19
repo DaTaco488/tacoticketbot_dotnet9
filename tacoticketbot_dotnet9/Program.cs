d12686d2d8771dd820e94bca527978a14a923a28
DaTaco <DaTaco@DATACO-PC2>
2026-09-18 20:06:06 -0400

    Add project files.

---
+++ tacoticketbot_dotnet9/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Windows.Forms;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace tacoticketbot_dotnet9
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
